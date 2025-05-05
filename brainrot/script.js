4400736f124424134f02149b7b3e1afaf5d27d47
unknown <radovan.marsalek@infis.cz>
2025-05-05 14:10:32 +0200

    aaa

---
+++ brainrot/script.js
@@ -0,0 +1,9 @@
+const img = document.createElement("img")
+img.src = "./shared image.jfif"
+
+const div = document.getElementById("div")
+
+const text = document.createElement("p")
+
+div.append(img)
+
