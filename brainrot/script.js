944966358ba53f3c6eaf52dcdac6ee3f32a84b4c
unknown <radovan.marsalek@infis.cz>
2025-05-12 13:54:28 +0200

    aaa

--- brainrot/script.js
+++ brainrot/script.js
@@ -3,7 +3,18 @@ img.src = "./shared image.jfif"
 
 const div = document.getElementById("div")
 
+const h1 = document.createElement("H1")
+
 const text = document.createElement("p")
+const text2 = document.createElement("p")
+const text3 = document.createElement("p")
 
 div.append(img)
-
+h1.textContent = "The grate devine god of cringe and prokrastination"
+ div.append(h1)
+text.textContent = "His divine duty is to spread procrastination throughout the human world."
+div.append(text)
+text2.textContent = "He procrastinates most of his time, and then takes credit for how many people procrastinate on their own (the other gods do not know this)"
+div.append(text2)
+text3.textContent = "Whenever he tries to tell a joke at the ten-decade meeting of the gods, any mood, good or bad, is immediately destroyed and replaced with a heavy atmosphere of cringe."
+div.append(text3)
